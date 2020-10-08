d3c90155c65a2aae6c1abd3711ab88d5eb624b33
nayankela <nayankela09@gmail.com>
2020-10-08 16:18:45 +0530

    done

--- src/app/contact-list/contact-list.component.ts
+++ src/app/contact-list/contact-list.component.ts
@@ -1,27 +1,39 @@
-import { contacts } from './../contact';
-import { userContact } from './../userContact';
-import { Component, OnInit } from '@angular/core';
-
+import { contacts } from "./../contact";
+import { userContact } from "./../userContact";
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'app-contact-list',
-  templateUrl: './contact-list.component.html',
-  styleUrls: ['./contact-list.component.css']
+  selector: "app-contact-list",
+  templateUrl: "./contact-list.component.html",
+  styleUrls: ["./contact-list.component.css"],
 })
 export class ContactListComponent implements OnInit {
   contacts: userContact[];
   newContact: any = {};
-  constructor() { }
+  newContactName = "";
+  newContactEmail = "";
+  newContactphoneNumber = "";
+  newContactImage = "";
+  constructor() {}
 
   ngOnInit() {
-  // initialize your contacts here
+    // initialize your contacts here
+    this.contacts = contacts;
   }
 
   addContact(newContact): any {
-    console.log('Add contact has been called');
-     // Fill your code here
+    console.log("Add contact has been called");
+    // Fill your code here
+    newContact = {
+      name: this.newContactName,
+      email: this.newContactEmail,
+      phoneNumber: this.newContactphoneNumber,
+      image: this.newContactImage,
+    };
     // add contact to contacts list
     // clear inputs
-   
+    console.log(newContact);
+    console.log(this.contacts);
+    this.contacts.unshift(newContact);
   }
 }
